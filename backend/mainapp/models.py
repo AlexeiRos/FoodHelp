6ba30754587609f7f527692bc75601bdc0f3a884
AlexeiRos <rostokin.s@mail.ru>
2021-03-10 21:16:05 +0300

    Pizdec v0.0.3

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -11,3 +11,41 @@ from django.urls import reverse
 from io import BytesIO
 
 User = get_user_model()
+
+
+class CategoryManager(models.Manager):
+
+    CATEGORY_NAME_COUNT_NAME = {
+        'Ингредиенты': 'ingredients__count'
+    }
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+    objects = CategoryManager()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Recipe(Product):
+
+    recipe_description = models.TextField(verbose_name='Описание рецепта', null=True)
+    calories = models.CharField(max_length=255, verbose_name='Калорийность')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
